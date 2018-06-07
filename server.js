de0bdcbf6ecee9322805d537ad639241241d1bc2
malikabhinav31 <malikabhinav31@gmail.com>
2018-06-07 16:40:12 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const hbs=require('hbs');
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+	var now=new Date().toString();
+	console.log(`${now}`);
+	next();
+});
+
+app.use((req,res,next)=>{
+  res.render("maintenance.hbs");
+});
+app.get('/',(response,request)=>{
+
+  request.render('home.hbs',{
+  	heading:'home page',
+  	message:'welcome to my website',
+  	year: new Date().getFullYear()
+  });
+});
+
+app.listen(3000);
