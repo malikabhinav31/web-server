dd4d9ded9955c38ca27cd112b7a5f0ac91ca382c
malikabhinav31 <malikabhinav31@gmail.com>
2018-06-08 17:08:25 +0530

    setup start & heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 
+const port=process.env.PORT|| 3000;
 var app=express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -24,4 +25,4 @@ app.get('/',(response,request)=>{
   });
 });
 
-app.listen(3000);
+app.listen(port);
